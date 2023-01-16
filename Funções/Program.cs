000069acf439f8e8d7345eb3e46feef9cba98a60
daniela marques <dd978452@gmail.com>
2023-01-16 12:29:36 -0300

    .

--- Funções/Program.cs
+++ Funções/Program.cs
@@ -5,11 +5,26 @@ namespace Funções{
     {
         static void Main(string[] args)
         {
+            /*
             GerarPreco(-60);//aqui no parenteses temos que colocar o valor do parametro
             GerarPreco(-95);
+            */
 
-            Console.ReadLine();
-        }
+            /*retorno de função
+            string valor = Console.ReadLine();// a função ReadLine executou, capturou a entrada do usuario
+            Console.WriteLine(valor); //capturou a entrada do usuario e retornou o valor que o usuario digitou para a variavel valor
+            Console.ReadLine();//e depois exibiu a mensagem valor 
+            */
+
+            int soma1 = Somar(1, 2, 3);
+            int soma2 = Somar(20, 30, 40);
+            int soma3 = Somar(100, 300, 400);
+
+            Console.WriteLine(soma1);
+            Console.WriteLine(soma2);
+            Console.WriteLine(soma3);
+
+            }
         static void ExibirMsg()
         {
             Console.WriteLine("Esse sistema é muito bom");
@@ -25,6 +40,14 @@ namespace Funções{
             int valorFinal = precoAbs + (2 * precoAbs);
             Console.WriteLine("Valor final: " + valorFinal);
         }
-            
+        //tipos de retorno: void, string, bool, float, int...
+        //o tipo de retorno sempre fica antes do nome da função
+        // quando define um tipo de retorno na função, é obrigatorio retornar algo dentro da sua funçao
+        //quando coloca void não é obrigatorio retornar nada
+        static int Somar(int a, int b, int c)
+        {
+            int resultadoFinal = a + b + c; 
+            return resultadoFinal;
+        }
     }
 }
